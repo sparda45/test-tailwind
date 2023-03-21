598cd6141eb213d9d1fe9114d97f031961135cbc
sparda45 <dyazkurosaki@gmail.com>
2023-03-22 00:52:00 +0700

    fitur_baru

--- script/script.js
+++ script/script.js
@@ -2,22 +2,38 @@
 window.onscroll = function(){
 
     const header = document.querySelector('header');
-    const fixednav =header.offsetTop;
+    const fixednav = header.offsetTop;
+    const totop = document.querySelector('#totop');
 
     if(window.pageYOffset > fixednav){
         header.classList.add('nav-fixed');
+        totop.classList.remove('hidden');
+        totop.classList.add('flex')
     } else{
         header.classList.remove('nav-fixed');
+        totop.classList.add('hidden');
+        totop.classList.remove('flex')
     }
 
 }
 
 // nav
-const nav = document.querySelector('#nav');
+const nav = document.querySelector('#nav')
 const NavMenu = document.querySelector('#nav-menu');
 
 nav.addEventListener('click', function(){
     nav.classList.toggle('nav-active');
     NavMenu.classList.toggle('hidden');
+
+});
+
+// nav ilang
+
+window.addEventListener('click', function(e){
+    if (e.target != nav && e.target != NavMenu ) {
+        nav.classList.remove('nav-active');
+        NavMenu.classList.add('hidden');
+    }
+
 
 });
